f6c90a4be5f9683f38a0a1e7ca574b66505b1930
Emil Gajšak <emil.gajsak@gmail.com>
2023-09-18 11:45:53 +0200

    Added WiFiAP random ID to ssid, setHidden method

--- main/src/Periph/WiFiAP.h
+++ main/src/Periph/WiFiAP.h
@@ -21,11 +21,19 @@ public:
 		};
 	};
 
+	/**
+	 * Set SSID broadcast on or off.
+	 * @param hidden True - hide SSID (default), False - broadcast SSID
+	 */
+	void setHidden(bool hidden);
+
 private:
 	esp_event_handler_instance_t evtHandler;
 	void event(int32_t id, void* data);
 
 	static esp_netif_t* createNetif();
+
+	const uint32_t randID;
 
 };
 
