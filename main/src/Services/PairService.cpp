8c8aac2f702aa654f597e02a150e12d356bdcaef
Emil Gajšak <emil.gajsak@gmail.com>
2023-09-19 11:19:15 +0200

    Added PairService

---
+++ main/src/Services/PairService.cpp
@@ -0,0 +1,28 @@
+#include "PairService.h"
+#include "Util/Services.h"
+#include "Util/Events.h"
+
+PairService::PairService() : Threaded("PairService", 4 * 1024),
+							 wifi(*(WiFiAP*) Services.get(Service{/*TODO - set actual service*/})),
+							 tcp(*(TCPServer*) Services.get(Service{/*TODO - set actual service*/})){
+//	wifi.setHidden(false);
+
+}
+
+PairService::~PairService(){
+//	wifi.setHidden(true);
+	if(running()){
+		stop();
+	}
+}
+
+void PairService::loop(){
+	bool accepted = tcp.accept();
+	if(accepted){
+		Event evt{ true };
+		Events::post(Facility::WiFiAP, evt);
+		stop();
+		return;
+	}
+	vTaskDelay(100 / portTICK_PERIOD_MS);
+}
