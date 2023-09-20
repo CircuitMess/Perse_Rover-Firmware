e8c0492bddfa88ab06d1e0e2eef6adbe6de01cf8
Emil Gajšak <emil.gajsak@gmail.com>
2023-09-20 16:30:35 +0200

    Comm class (#6)
    * Added Perse-Common to components
    
    * Added Comm class, TCPServer connect/disconnect events
    
    * Moved Comm::sendPacket to private
    
    * Added missing Comm::start in constructor
    
    * Perse-Common submodule renamings
    
    * Updated Perse-Common
    
    * Implemented Comm model for DriveDir in Comm, using TCP service
    
    * Added CommType to Comm:Event struct
    
    * Added tcp read result checks in Comm
    
    * Removed unsafe comment from Comm

---
+++ main/src/Services/Comm.cpp
@@ -0,0 +1,50 @@
+#include "Comm.h"
+#include "Util/Services.h"
+
+Comm::Comm() : Threaded("Comm", 4 * 1024), tcp(*(TCPServer*) Services.get(Service::TCP)), queue(10){
+	Events::listen(Facility::TCP, &queue);
+	start();
+}
+
+Comm::~Comm(){
+	Events::unlisten(&queue);
+	stop();
+}
+
+void Comm::sendPacket(const ControlPacket& packet){
+	if(!tcp.isConnected()) return;
+
+	tcp.write((uint8_t*) &packet, sizeof(ControlPacket));
+}
+
+void Comm::loop(){
+	bool readOK = false;
+	if(tcp.isConnected()){
+		ControlPacket packet{};
+		readOK = tcp.read(reinterpret_cast<uint8_t*>(&packet), sizeof(ControlPacket));
+
+		if(readOK){
+			Event e = processPacket(packet);
+			Events::post(Facility::Comm, e);
+		}
+	}
+
+	if(!tcp.isConnected() || !readOK){
+		::Event event{};
+		while(!queue.get(event, portMAX_DELAY));
+		free(event.data);
+	}
+}
+
+Comm::Event Comm::processPacket(const ControlPacket& packet){
+	Event e{};
+	e.raw = packet.data;
+	e.type = packet.type;
+	switch(packet.type){
+		case CommType::DriveDir:
+			e.dir = CommData::decodeDriveDir(packet.data);
+			break;
+	}
+
+	return e;
+}
---
+++ main/src/Services/Comm.h
@@ -0,0 +1,32 @@
+#ifndef PERSE_ROVER_COMM_H
+#define PERSE_ROVER_COMM_H
+
+#include "TCPServer.h"
+#include "Util/Threaded.h"
+#include "Util/Events.h"
+#include <CommData.h>
+
+class Comm : private Threaded {
+public:
+	struct Event {
+		CommType type;
+		union {
+			DriveDir dir;
+		};
+		uint8_t raw;
+	};
+
+	Comm();
+	~Comm() override;
+
+private:
+	TCPServer& tcp;
+	void loop() override;
+	void sendPacket(const ControlPacket& packet);
+	Event processPacket(const ControlPacket& packet);
+
+	EventQueue queue;
+};
+
+
+#endif //PERSE_ROVER_COMM_H
