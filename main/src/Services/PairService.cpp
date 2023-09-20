9be09dfceab0ed0e4018ee3fe1c82d65ae8a4643
Emil Gajšak <emil.gajsak@gmail.com>
2023-09-20 14:37:43 +0200

    Refactors, cleanup in PairService

--- main/src/Services/PairService.cpp
+++ main/src/Services/PairService.cpp
@@ -1,19 +1,18 @@
 #include "PairService.h"
 #include "Util/Services.h"
 #include "Util/Events.h"
+#include "Util/stdafx.h"
 
 PairService::PairService() : Threaded("PairService", 4 * 1024),
-							 wifi(*(WiFiAP*) Services.get(Service{/*TODO - set actual service*/})),
-							 tcp(*(TCPServer*) Services.get(Service{/*TODO - set actual service*/})){
-//	wifi.setHidden(false);
+							 wifi(*(WiFiAP*) Services.get(Service::WiFi)),
+							 tcp(*(TCPServer*) Services.get(Service::TCP)){
+	wifi.setHidden(false);
 
 }
 
 PairService::~PairService(){
-//	wifi.setHidden(true);
-	if(running()){
-		stop();
-	}
+	wifi.setHidden(true);
+	stop();
 }
 
 void PairService::loop(){
@@ -24,5 +23,5 @@ void PairService::loop(){
 		stop();
 		return;
 	}
-	vTaskDelay(100 / portTICK_PERIOD_MS);
+	delayMillis(100);
 }
