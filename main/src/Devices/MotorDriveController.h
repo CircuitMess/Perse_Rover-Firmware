96e5292df3a1488f63d151898003dc7417b91ca0
Fr0zen3k <jan.kresic@circuitmess.com>
2023-10-18 09:55:07 +0200

    Refactor

--- main/src/Devices/MotorDriveController.h
+++ main/src/Devices/MotorDriveController.h
@@ -19,7 +19,7 @@ protected:
 	virtual void write(const MotorDriveState& state) override;
 	virtual MotorDriveState getDefaultState() const override;
 	virtual void sendState(const MotorDriveState& state) const override;
-	virtual MotorDriveState processStateFromEvent(const Event& event) const override;
+	virtual void processEvent(const Event& event) override;
 
 private:
 	class MotorControl* motorControl = nullptr;
