07086b573029d781e7e7a73f80d7360a456cdb1b
Filip Budiša <filip@circuitmess.com>
2023-11-02 11:27:19 +0100

    Init: Create device controllers before StateMachine

--- main/main.cpp
+++ main/main.cpp
@@ -74,16 +74,16 @@ void init(){
 	auto comm = new Comm();
 	Services.set(Service::Comm, comm);
 
+	auto headlightsController = new HeadlightsController(*aw9523);
+	auto motorDriveController = new MotorDriveController();
+	auto armController = new ArmController();
+	auto cameraController = new CameraController();
+
 	auto stateMachine = new StateMachine();
 	Services.set(Service::StateMachine, stateMachine);
 
 	stateMachine->transition<PairState>();
 	stateMachine->begin();
-
-	auto headlightsController = new HeadlightsController(*aw9523);
-	auto motorDriveController = new MotorDriveController();
-	auto armController = new ArmController();
-	auto cameraController = new CameraController();
 
 	battery->begin();
 }
