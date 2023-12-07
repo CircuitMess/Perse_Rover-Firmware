a37e23a2bfb3067382dce882c4b4daeb3fa8d112
Fr0zen3k <jan.kresic@circuitmess.com>
2023-12-07 09:57:57 +0100

    Fixed unneeded not found error logging in camera

--- main/src/Devices/Camera.cpp
+++ main/src/Devices/Camera.cpp
@@ -65,7 +65,7 @@ esp_err_t Camera::init(){
 	auto lock = i2c.lockBus();
 
 	auto err = esp_camera_init(&config);
-	if(err == ESP_ERR_CAMERA_NOT_DETECTED){
+	if(err == ESP_ERR_NOT_FOUND){
 		return err;
 	}else if(err != ESP_OK){
 		printf("Camera init failed with error 0x%x: %s\n", err, esp_err_to_name(err));
