c90033fd993f9b4e4653873a78b813ede3e4fc74
Fr0zen3k <jan.kresic@circuitmess.com>
2024-03-26 15:39:49 +0100

    Jig hardware test WIP

---
+++ main/src/JigHWTest/SPIFFSChecksum.hpp
@@ -0,0 +1,11 @@
+#ifndef SPIFFSCHECKSUM_HPP
+#define SPIFFSCHECKSUM_HPP
+
+struct {
+	const char* name;
+	uint32_t sum;
+} static const SPIFFSChecksums[] PROGMEM = {
+	{ "/audioOn.wav", 13347341},
+};
+
+#endif //SPIFFSCHECKSUM_HPP
